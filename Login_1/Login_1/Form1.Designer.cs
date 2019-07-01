a9749224fb4d98457c3aa22976c2c5aa86898e5d
AndreaGarces <49161324+AndreaGarces@users.noreply.github.com>
2019-06-30 21:23:00 -0500

    Pruebas

---
+++ Login_1/Login_1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Login_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.Mostrar = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(64, 56);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 0;
+            this.Usuario.Text = "Usuario";
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.AutoSize = true;
+            this.Contraseña.Location = new System.Drawing.Point(64, 103);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.Contraseña.TabIndex = 1;
+            this.Contraseña.Text = "Contraseña";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(128, 56);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(164, 20);
+            this.textBoxUsuario.TabIndex = 2;
+            this.textBoxUsuario.TextChanged += new System.EventHandler(this.TextBoxUsuario_TextChanged);
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.Location = new System.Drawing.Point(128, 103);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.Size = new System.Drawing.Size(164, 20);
+            this.textBoxContraseña.TabIndex = 3;
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.Location = new System.Drawing.Point(128, 165);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.BtnIngresar.TabIndex = 4;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // Mostrar
+            // 
+            this.Mostrar.AutoSize = true;
+            this.Mostrar.Location = new System.Drawing.Point(128, 129);
+            this.Mostrar.Name = "Mostrar";
+            this.Mostrar.Size = new System.Drawing.Size(61, 17);
+            this.Mostrar.TabIndex = 5;
+            this.Mostrar.Text = "Mostrar";
+            this.Mostrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 236);
+            this.Controls.Add(this.Mostrar);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.Contraseña);
+            this.Controls.Add(this.Usuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label Contraseña;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private System.Windows.Forms.Button BtnIngresar;
+        private System.Windows.Forms.CheckBox Mostrar;
+    }
+}
+
